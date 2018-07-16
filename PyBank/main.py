2c339b79ba0a8c3eb9dd83b30c65347c7d0b72c1
Rose Wachira <waridicook@gmail.com>
2018-07-16 16:03:07 -0500

    Changes to Folders

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,116 @@
+# pybank Homework Rose Wachira
+
+# Import modules
+
 import csv
+import os
 
-csvpath = "Resources/budget_data.csv"
-with open('csvpath','r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+# Create path for files
+budget = os.path.join('Resources','budget_data.csv')
+analysis = os.path.join('Resources','analysis_data.text')
 
-    for line in csv_reader:
-        print(line)
+ 
+ # define variables
+NumMonths = 0
+Revenue = 0
+TTRevenue = 0
+GProfit_decrease = 0
+GProfit_increase = 0
+Month_GIncrease = ""
+Month_GDecrease = ""
+title = str("Financial Analysis")
+ 
+ 
+with open(budget) as budget_data:
+
+    budget_data_read = csv.reader(budget_data)
+    next(budget_data_read)
+ 
+    for row in budget_data_read:
+
+        NumMonths+=1
+
+        CurMonth = row[0]
+
+        CurRevenue = int(row[1])
+
+ 
+        RevenueChange= CurRevenue - Revenue
+
+        if RevenueChange > GProfit_increase:
+
+            Month_GIncrease = CurMonth
+
+            GProfit_increase = RevenueChange
+
+        if RevenueChange < GProfit_decrease:
+
+            Month_GDecrease = CurMonth
+
+            GProfit_decrease = RevenueChange
+
+ 
+        Revenue = CurRevenue
+
+        TTRevenue += CurRevenue
+
+ 
+
+    AvgRevenue = TTRevenue/NumMonths
+
+    AvgRev2= round(AvgRevenue,2)
+
+    #print(AvgRev2)
+
+
+print("")
+
+print(f"{title}")
+
+print("-----------------------------------------------")
+
+print(f"Total  Months: {NumMonths}")
+
+print(f"Total: ${TTRevenue}")
+
+print(f"Average Change: ${AvgRev2}")
+
+print(f"Greatest Increase in Revenue: {Month_GIncrease}  (${GProfit_increase}) ")
+
+print(f"Greatest Decrease in Revenue: {Month_GDecrease} (${GProfit_decrease})")
+
+print("------------------------------------------------")
+
+ 
+
+output = (
+
+
+    f"\n\n"
+
+    f"\n{title}\n"
+
+    f"\n-----------------------------------------------\n"
+
+    f"\nTotal Number of Months: {NumMonths} months\n"
+
+    f"\nTotal Revenue: ${TTRevenue}\n"
+
+    f"\nAverage Change: ${AvgRev2}\n"
+
+    f"\nGreatest Increase in Revenue: {Month_GIncrease} ${GProfit_increase}\n"
+
+    f"\nGreatest Decrease in Revenue: {Month_GDecrease} ${GProfit_decrease}\n"
+
+    f"\n------------------------------------------------\n")
+
+#print(output)
+
+with open(analysis, "w") as txt_file:
+
+    txt_file.write(output)
+
+ 
+
+ 
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,65 @@
+# PyPoll Homework Rose Wachira
+#import modules
 import csv
 import os
 
-csvPath = "Resources\election_data.csv"
+# set the path of the file
+csvpath = os.path.join('Resources', 'election_data.csv') 
 
-with open('csvPath', 'r')as csv_file:
-    csv_reader = csv.reader(csv_file)
+#variables
+votes_cast = 0
+Candidates = {}
+Candidates_percent = {}
+election_winner = ""
+election_winnerCount = 0
+
+#open the file
+with open(csvpath,newline = "")as csvfile:
+    csv_reader=csv.reader(csvfile, delimiter = ",")
+    next(csv_reader,None)
     
-    for line in csv_reader:
-        print(line)
-        
+#Formula to find each candidates votes and total of votes cast    
+    for row in csv_reader:
+        votes_cast += 1
+        if row[2] in Candidates.keys():
+            Candidates[row[2]] += 1
+        else:
+            Candidates[row[2]] = 1
+#print(votes_cast) 
+  
+#calculating percentage for each canditate
+for key, value in Candidates.items():
+    Candidates_percent[key] = round((value/votes_cast)*100,2)
+#print(Candidates_percent) 
+
+#Identifying the winner in elections
+for key in Candidates.keys():
+    if Candidates[key]> election_winnerCount:
+        winner = key
+        election_winnerCount = Candidates[key]
+#print(winner)  
+
+
+#print script
+print("Election Results")
+print("------------------------------------------")
+print("Total:" + str(votes_cast))
+print("------------------------------------------")
+print(key + ":" + str(Candidates_percent[key]) + "%(" + str(value) +")")
+print("-------------------------------------------")
+print("Winner:" + winner)
+print("-------------------------------------------")
+
+#Text File
+PyPoll = open("poll_results.txt","w")
+
+#Writing PyPoll Text File
+PyPoll.write("Election Results \n")
+PyPoll.write("------------------------\n")
+PyPoll.write("Total:" + str(votes_cast) + "\n")
+PyPoll.write("-------------------------\n")
+for key, value in Candidates.items():
+    PyPoll.write(key + ":" + str(Candidates_percent[key]) +"%(" + str(value)+ ")\n")
+PyPoll.write("--------------------------\n")
+PyPoll.write("Winner:" + winner + "\n")
+PyPoll.write("--------------------------\n")
